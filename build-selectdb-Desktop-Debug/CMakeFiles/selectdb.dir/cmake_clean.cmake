51906b5506ccaa25f347a1025414b1954db1ce98
Satrana Ravelojaona <satranaraveelojaona@gmail.com>
2023-02-01 14:40:09 +0100

    add selectdb

---
+++ build-selectdb-Desktop-Debug/CMakeFiles/selectdb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/selectdb.dir/main.c.o"
+  "CMakeFiles/selectdb.dir/main.c.o.d"
+  "selectdb"
+  "selectdb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/selectdb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-selectdb-Desktop-Debug/CMakeFiles/selectdb.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for selectdb.
+# This may be replaced when dependencies are built.
